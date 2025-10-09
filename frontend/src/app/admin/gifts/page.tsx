e6e92f5400a1ae3755994ab5a1a392bc31c08f6a
Jefferson Timotius Mansur <122242388+JeffTimoman@users.noreply.github.com>
2025-10-09 23:57:38 +0700

    patch 11

--- frontend/src/app/admin/gifts/page.tsx
+++ frontend/src/app/admin/gifts/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import api, { Gift } from '../../../../lib/api/gifts';
+import api, { Gift } from '../../../lib/api/gifts';
 
 export default function GiftsAdmin() {
   const [gifts, setGifts] = useState<Gift[]>([]);
